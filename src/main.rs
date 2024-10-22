68eb206d76b44163fc6fd9cbda5600f9d1c2d9a8
Thomas <trbiv@proton.me>
2024-10-21 21:16:01 -0500

    update server

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use oauth2::PkceCodeVerifier;
+use std::sync::Mutex;
 
 mod oauth_client;
 
-#[get("/")]
+static PKCE_CHALLENGE: Mutex<Option<oauth2::PkceCodeChallenge>> = Mutex::new(None);
+static PKCE_VERIFIER: Mutex<Option<PkceCodeVerifier>> = Mutex::new(None);
+
+#[get("/bah")]
 async fn bah() -> impl Responder {
     format!("bahhhhhhh")
 }
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+#[get("/connect")]
+async fn connect() -> impl Responder {
+    println!("Attemptin to create client");
+    let client = oauth_client::create_client().unwrap();
+
+    let (pkce_challenge, pkce_verifier) = oauth_client::genetate_challenge();
+
+    *PKCE_CHALLENGE.lock().unwrap() = Some(pkce_challenge.clone());
+    *PKCE_VERIFIER.lock().unwrap() = Some(pkce_verifier);
+
+    let (auth_url, csrf_token) = oauth_client::generate_authorization_url(client, pkce_challenge);
+
+    println!("Connect to {}", auth_url);
+
+    format!("Connet to: {}", auth_url)
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
+    HttpServer::new(|| App::new().service(bah).service(connect))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
--- src/oauth_client.rs
+++ src/oauth_client.rs
@@ -1,28 +1,33 @@
 use anyhow;
-use oauth2::basic::BasicClient;
+use oauth2::basic::{BasicClient, BasicTokenType};
 use oauth2::reqwest::async_http_client;
 use oauth2::{
-    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl,
-    RequestTokenError, Scope, TokenResponse, TokenUrl,
+    AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, EmptyExtraTokenFields,
+    PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, RequestTokenError, Scope,
+    StandardTokenResponse, TokenResponse, TokenUrl,
 };
 use url::Url;
 
-async fn get_token() -> Result<(), anyhow::Error> {
-    // Create an OAuth2 client by specifying the client ID, client secret, authorization URL and
-    // token URL.
+pub fn create_client() -> Result<BasicClient, anyhow::Error> {
     let client = BasicClient::new(
         ClientId::new("client_id".to_string()),
         Some(ClientSecret::new("client_secret".to_string())),
         AuthUrl::new("http://authorize".to_string())?,
         Some(TokenUrl::new("http://token".to_string())?),
     )
-    // Set the URL the user will be redirected to after the authorization process.
     .set_redirect_uri(RedirectUrl::new("http://redirect".to_string())?);
 
-    // Generate a PKCE challenge.
-    let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+    Ok(client)
+}
 
-    // Generate the full authorization URL.
+pub fn genetate_challenge() -> (PkceCodeChallenge, PkceCodeVerifier) {
+    PkceCodeChallenge::new_random_sha256()
+}
+
+pub fn generate_authorization_url(
+    client: BasicClient,
+    pkce_challenge: PkceCodeChallenge,
+) -> (url::Url, CsrfToken) {
     let (auth_url, csrf_token) = client
         .authorize_url(CsrfToken::new_random)
         // Set the desired scopes.
@@ -32,15 +37,13 @@ async fn get_token() -> Result<(), anyhow::Error> {
         .set_pkce_challenge(pkce_challenge)
         .url();
 
-    // This is the URL you should redirect the user to, in order to trigger the authorization
-    // process.
-    println!("Browse to: {}", auth_url);
+    (auth_url, csrf_token)
+}
 
-    // Once the user has been redirected to the redirect URL, you'll have access to the
-    // authorization code. For security reasons, your code should verify that the `state`
-    // parameter returned by the server matches `csrf_state`.
-
-    // Now you can trade it for an access token.
+pub async fn get_access_token(
+    client: BasicClient,
+    pkce_verifier: PkceCodeVerifier,
+) -> Result<StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>, anyhow::Error> {
     let token_result = client
         .exchange_code(AuthorizationCode::new(
             "some authorization code".to_string(),
@@ -50,8 +53,5 @@ async fn get_token() -> Result<(), anyhow::Error> {
         .request_async(async_http_client)
         .await?;
 
-    // Unwrapping token_result will either produce a Token or a RequestTokenError.
-    //
-    //
-    Ok(())
+    Ok(token_result)
 }
